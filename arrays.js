d9bad777d7b3ff22983e897319aebe257c4bef87
Jan Spencer Catalan <jancatalan2@gmail.com>
2018-09-25 03:34:43 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = 
+[
+  'snickers',
+  'kitkat',
+  'skittles'
+];
+function addElementToBeginningOfArray(array, element)
+{
+ var array2 = [element, ...array]
+ return array2;
+}
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+ array.unshift(element);
+ return array;
+}
+function addElementToEndOfArray(array, element) {
+  var array2 = [...array, element]
+  return array2;
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+function accessElementInArray(array, index) {
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+function removeElementFromBeginningOfArray(array) {
+ var array2 = array.slice(1);
+  return array2;
+}
